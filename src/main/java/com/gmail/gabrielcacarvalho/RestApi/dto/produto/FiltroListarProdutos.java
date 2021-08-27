1498a60ad995aeb9e0c03b0f64093bb7a9607b51
Gabriel Cardoso de Araújo Carvalho <gabrielcacarvalho@gmail.com>
2021-08-26 22:41:33 -0300

    Feature: Terminada a implementacao do filtro de produtos.

--- src/main/java/com/gmail/gabrielcacarvalho/RestApi/dto/produto/FiltroListarProdutos.java
+++ src/main/java/com/gmail/gabrielcacarvalho/RestApi/dto/produto/FiltroListarProdutos.java
@@ -4,6 +4,7 @@ import com.gmail.gabrielcacarvalho.RestApi.core.entity.enumerator.Tamanho;
 import com.gmail.gabrielcacarvalho.RestApi.core.entity.enumerator.Categoria;
 import com.gmail.gabrielcacarvalho.RestApi.core.entity.model.Marca;
 import com.gmail.gabrielcacarvalho.RestApi.core.entity.model.Promocao;
+import com.gmail.gabrielcacarvalho.RestApi.core.entity.model.TipoVestimenta;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -11,10 +12,10 @@ import java.math.BigDecimal;
 @Data
 public class FiltroListarProdutos {
 
-    //private TipoVestimenta tipo;
+    private Integer idTipoVestimenta;
     private Categoria categoria;
-    private Marca marca;
-    private Promocao promocao;
+    private Integer idMarca;
+    private Integer idPromocao;
     private String cor;
     private Tamanho tamanho;
     private BigDecimal precoMin;
--- src/main/java/com/gmail/gabrielcacarvalho/RestApi/service/ProdutoService.java
+++ src/main/java/com/gmail/gabrielcacarvalho/RestApi/service/ProdutoService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import javax.persistence.criteria.Predicate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProdutoService {
@@ -23,11 +24,33 @@ public class ProdutoService {
         return produtoRepository.findAll(criarFiltroBuscarLista(filtros), pageable);
     }
 
-    private Specification<Produto> criarFiltroBuscarLista(FiltroListarProdutos filtros) {
+    private Specification<Produto> criarFiltroBuscarLista(FiltroListarProdutos filtroListarProdutos) {
+
+        FiltroListarProdutos filtros = Optional.ofNullable(filtroListarProdutos).orElse(new FiltroListarProdutos());
+
         return (root, query, builder)->{
             List<Predicate> predicates = new ArrayList<>();
 
+            if(filtros.getIdTipoVestimenta() != null)
+                predicates.add(builder.equal(root.get("tipoVestimenta").get("id"), filtros.getIdTipoVestimenta()));
 
+            if (filtros.getCategoria() != null)
+                predicates.add(builder.equal(root.get("categoria"), filtros.getCategoria()));
+
+            if (filtros.getCor() != null)
+                predicates.add(builder.equal(root.get("cor"), filtros.getCor()));
+
+            if (filtros.getIdMarca() != null)
+                predicates.add(builder.equal(root.get("marca").get("id"), filtros.getIdMarca()));
+
+            if (filtros.getIdPromocao() != null)
+                predicates.add(builder.equal(root.get("promocao").get("id"), filtros.getIdPromocao()));
+
+            if (filtros.getTamanho() != null)
+                predicates.add(builder.equal(root.get("tamanho"), filtros.getTamanho()));
+
+            if (filtros.getPrecoMin() != null && filtros.getPrecoMax() != null)
+                predicates.add(builder.between(root.get("valorUnitario"), filtros.getPrecoMin(), filtros.getPrecoMax()));
 
             return builder.and(predicates.toArray(new Predicate[0]));
         };
